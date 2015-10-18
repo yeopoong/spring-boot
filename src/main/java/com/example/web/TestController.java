09dbc73ef4281315e867129208cb1543a9c8cc00
yeopoong <yeopoong@gmail.com>
2015-10-18 21:31:27 +0900

    first commit

---
+++ src/main/java/com/example/web/TestController.java
@@ -0,0 +1,21 @@
+package com.example.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class TestController {
+	
+	@RequestMapping("/")
+	@ResponseBody
+	String home() {
+		return "Hello World!";
+	}
+
+	@RequestMapping("/test")
+	String body() {
+		return "test/test";
+	}
+
+}
